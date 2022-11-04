d8abd2a51e72ec94fed958fcd4aadec97353e5b5
tamuramasumi <tngu09010108@gmail.com>
2022-11-04 12:20:28 +0000

    サンプルページのURLを変更した

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 */
-Route::get('/', [WelcomeController::class, 'index']);
+Route::get('/welcome', [WelcomeController::class, 'index']);
 Route::get('/welcome/second',[WelcomeController::class, 'second']);
 
